6e90ba620f31c1417b8b8e908be45dfa59d993b3
ISAAC <sapoalado070@gmail.com>
2024-07-23 23:04:08 -0300

    login e cadastro concluido.1.2

--- pyutils-front/main.js
+++ pyutils-front/main.js
@@ -3,13 +3,16 @@ async function iniciar(){
     if (!(sessionStorage.getItem("token")==null)){
         var nome = sessionStorage.getItem('username')
         div.innerHTML = `Olá, ${nome}`
+        console.log('token')
     }else{
         var link = document.createElement("a")
         var btn = document.createElement("button")
         btn.innerText = 'cadstrar/logar'
         link.href = 'https://pyutilidades.vercel.app/auth/cadastro.html'
         link.appendChild(btn)
+        console.log('login-dado')
     }
 
 }
+console.log('iniciar')
 iniciar()
